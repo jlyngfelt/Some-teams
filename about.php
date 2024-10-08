2b9a83fa43311590317c37c18010322c71ab4443
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2024-10-08 09:52:19 +0200

    Last changes and cleaning
    Did som last changes and some cleaning in the code.

--- about.php
+++ about.php
@@ -7,8 +7,8 @@
 </head>
 <body>
     <?php
-require_once __DIR__ ."/header.php";
-?>
+    require_once __DIR__ ."/header.php";
+    ?>
     
 <main class="main-about">
     <img src="Julia.png" alt="Picture of Julia">
@@ -16,9 +16,8 @@ require_once __DIR__ ."/header.php";
         <p>First of all, I want to tell you that I rather speak about my myself in Swedish, that is why jag kommer fortsätta på svenska från nu och vidare. </p>
         <p class="body-text">För att ge en rättvis, och ändå lite fotbollsknuten, bild av mig, så tänker jag börja med att berätta att jag är ifrån Varberg, Hallands Hawaii, eller Boråsarnas Bahamas. Det är nämligen så att varje sommar vallfärdar större delen av Borås befolkning sydväst, och på Varbergs torg blir det plötsligt svårt att hitta någon som faktiskt kommer ifrån stan. </p>
         <p class="body-text">Jaha tänker du, då håller hon på Varbergs BoIS. Tyvärr måste jag göra din slutledningsförmåga lika besviken som jag gjorde min pappa när jag ringde hem och berättade att jag börjat följa allsvenskan. Pappa tjoade och sa, “Vad kul! Då kan vi heja fram de grön/svarta färgerna ihop!”. Nej kära pappa fick jag svara, jag har valt att gå med ett annat lag. Det är nämligen så att jag har gått och blivit Elfsborgare, precis som alla andra som vistas i Varberg på somrarna. </p>
-    <p class="body-text">Jag är alltså en 29 årig tjej som ser på fotboll ibland och är boende i masthugget i Göteborg. Där bor jag med min sambo Alfred, som såklart är skyldig till ovanstående svek. Annars på fritiden lyssnar jag på ohälsosamma mängder podd, är ute och springer och dricker ett och annat glas vin med mina kompisar. </p></div>
+        <p class="body-text">Jag är alltså en 29 årig tjej som ser på fotboll ibland och bor i Masthugget i Göteborg. Där bor jag med min sambo Alfred, som såklart är skyldig till ovanstående svek. Annars på fritiden lyssnar jag på ohälsosamma mängder podd, är ute och springer och dricker ett och annat glas vin med mina kompisar. </p></div>
 </main>
-    
 
 </body>
 </html>
