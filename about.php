93effd7ffcc13a2dd3a7120005621c9688c7b6dc
jlyngfelt <168407644+jlyngfelt@users.noreply.github.com>
2024-10-21 14:33:31 +0200

    put php over html in about.php

--- about.php
+++ about.php
@@ -1,3 +1,7 @@
+    <?php
+    require_once __DIR__ ."/header.php";
+    ?>
+    
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,9 +10,6 @@
     <title>About me</title>
 </head>
 <body>
-    <?php
-    require_once __DIR__ ."/header.php";
-    ?>
     
 <main class="main-about">
     <img src="Julia.png" alt="Picture of Julia">
